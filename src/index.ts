447e667535c6f8189b449003e846cc10b166066a
huyong <1098015278@qq.com>
2018-12-25 15:35:42 +0800

    @nestsoft/config放到生产环境依赖中

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,13 @@ import Chalk from "chalk";
 import * as net from "net";
 import { Models } from "./model";
 import Router from "./router";
+import * as bodyParser from 'koa-bodyparser';
 
 (async () => {
     const app = new Koa();
     console.log(process.env.SERVER_NAME);
     app.use(Router);
+    app.use(bodyParser());
     await Models.sync({ force: false });
     // start server
     const server = http.createServer(app.callback());
